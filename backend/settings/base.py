23eac3931b6e07cb4c04e2f8fb1b36a53c38a69b
Ahmed Rayan <101011496+Rayan420@users.noreply.github.com>
2024-02-24 17:00:12 +0300

    project setup

--- backend/settings/base.py
+++ backend/settings/base.py
@@ -1,5 +1,10 @@
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
+
+# Load environment variables from .env file
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -8,11 +13,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-f35cm-fx(_3&eb9@^zrdrbkux$356wfe+5$j+kf5p8og&x)%-v'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+SECRET_KEY = os.environ.get('SECRET_KEY')
+DEBUG = os.environ.get('DEBUG')
 
 ALLOWED_HOSTS = []
 
